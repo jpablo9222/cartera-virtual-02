b9f70be39e28d54eadc1a4d987625fc5648b597e
jmunoz1991@gmail.com <jmunoz1991@gmail.com@709e4cd4-b995-ec9b-8143-b2fa3366bc6b>
2011-10-27 17:59:15 +0000

    Creación de esqueletos para clases

---
+++ trunk/Proyecto2/src/AplicacionesInternet.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tono
+ */
+public class AplicacionesInternet extends Cuenta {
+    private String link;
+    public AplicacionesInternet(String titulo,String usuario, String pass,String link){
+        super(titulo,usuario,pass);
+    }
+    public String getLink(){
+        return link;
+    }
+}
---
+++ trunk/Proyecto2/src/Membresia.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Tono
+ */
+import java.util.Date;
+public class Membresia extends Cuenta{
+    private Date fechaExp,fechaAd;
+    
+    public Membresia(String titulo, String usuario,String pass, Date fecha1,Date fecha2){
+        super(titulo,usuario,pass);
+        fechaAd=fecha1;
+        fechaExp=fecha2;
+    }
+    public Date getFechaExp(){
+        return fechaExp;
+    }
+    public Date getFechaAd(){
+        return fechaAd;
+    }
+}
