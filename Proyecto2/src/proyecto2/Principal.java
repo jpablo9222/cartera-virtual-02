c04483d96f55075d65e7884f3b9f9678f8ead5cb
jp_9222 <jp_9222@hotmail.com>
2011-10-31 05:49:04 +0000

    Se arreglaron parametros de métodos

--- Proyecto2/src/proyecto2/Principal.java
+++ Proyecto2/src/proyecto2/Principal.java
@@ -11,21 +11,21 @@ package proyecto2;
  */
 import java.util.ArrayList;
 public class Principal {
-    private ArrayList <AplicacionesIntenet> listAppI=new ArrayList();
-    private ArrayList <DispositivosE> listDispE=new ArrayList();
-    private ArrayList <Membresia> listMembrecia =new ArrayList();
-    private ArrayList <ProductosInfo> listProdInfo =new ArrayList();
+    private static ArrayList <AplicacionesInternet> listAppI=new ArrayList();
+    private static ArrayList <DispositivosE> listDispE=new ArrayList();
+    private static ArrayList <Membresia> listMembresia =new ArrayList();
+    private static ArrayList <ProductosInfo> listProdInfo =new ArrayList();
     
-    public static void addAppI(Cuenta AplicacionesInternet){
+    public static void addAppI(AplicacionesInternet AplicacionesInternet){
             listAppI.add(AplicacionesInternet);
     }
-    public static void addDispE(Cuenta DispositivosE){
+    public static void addDispE(DispositivosE DispositivosE){
         listDispE.add(DispositivosE);
     }
-    public static void addMembresia(Cuenta Membresia){
+    public static void addMembresia(Membresia Membresia){
         listMembresia.add(Membresia);
     }
-    public static void addProdInfo(Cuenta ProductosInfo){
+    public static void addProdInfo(ProductosInfo ProductosInfo){
         listProdInfo.add(ProductosInfo);
     }
     public static void main(String[] args) {
