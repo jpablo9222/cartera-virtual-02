b5798de34ea96d74259019164090dcc4d1d4c6e7
jp_9222 <jp_9222@hotmail.com>
2011-11-12 08:17:59 +0000

    GUI

--- Proyecto2/src/MySQL/GUICartera.java
+++ Proyecto2/src/MySQL/GUICartera.java
@@ -1,13 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * GUICartera.java
- *
- * Created on 11/11/2011, 11:32:03 PM
- */
 package MySQL;
 
 /**
