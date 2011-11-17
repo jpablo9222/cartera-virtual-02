0206cbd073faf69c2e00afd83f849343ff7d5615
jp_9222@hotmail.com <jp_9222@hotmail.com@709e4cd4-b995-ec9b-8143-b2fa3366bc6b>
2011-11-17 02:50:34 +0000

    Desarrollo GUI

--- trunk/Proyecto2/src/MySQL/CambiarPass.java
+++ trunk/Proyecto2/src/MySQL/CambiarPass.java
@@ -1,13 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * CambiarPass.java
- *
- * Created on 16/11/2011, 01:12:41 AM
- */
 package MySQL;
 
 /**
